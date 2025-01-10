1576f853b448459c0faa4872d512a9d31c042037
Rasmus <rasmusjensen.dev@gmail.com>
2025-01-10 18:48:07 +0100

    Made some routing changes

--- laravel-client/app/Http/Controllers/LoginController.php
+++ laravel-client/app/Http/Controllers/LoginController.php
@@ -3,14 +3,31 @@
 namespace App\Http\Controllers;
 
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    // Show the login page
-    public function index()
+    public function index(Request $request)
     {
-        return view('login'); // Show the login view
+        // Check if the user session exists
+        if ($request->session()->has('google_user')) {
+            // If the user session exists, redirect to the dashboard
+            return redirect('/dashboard');
+        }
+
+        // Otherwise, redirect to the login page
+        return redirect('/login');
     }
+
+    public function showLoginForm(Request $request)
+{
+    // Clear the session
+    $request->session()->forget('google_user');
+
+    // Show the login view
+    return view('login');
+}
+
 
     // Redirect to Google's OAuth page
     public function redirectToGoogle()
--- laravel-client/routes/web.php
+++ laravel-client/routes/web.php
@@ -4,13 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Middleware\CheckLogin;
 
-Route::get('/', [LoginController::class, 'index'])->name('login');
-Route::get('auth/google', [LoginController::class, 'redirectToGoogle']);
-Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
+Route::get('/', [LoginController::class, 'index']);
 
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
 Route::get('/dashboard', function () {
     $user = session('google_user');
     return view('dashboard', ['userName' => $user['name']]);
 })->middleware(CheckLogin::class);
 
+
+Route::get('auth/google', [LoginController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
