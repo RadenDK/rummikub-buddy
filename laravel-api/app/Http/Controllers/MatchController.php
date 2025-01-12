5e65b4ff6d96d63070bd16cbad18c4e57147fe9e
Rasmus <rasmusjensen.dev@gmail.com>
2025-01-12 09:34:17 +0100

    Added simple initial stuff just testing out the routes

---
+++ laravel-api/app/Http/Controllers/MatchController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatchController extends Controller
+{
+    public function saveMatch()
+    {
+        return "Match saved";
+    }
+}
