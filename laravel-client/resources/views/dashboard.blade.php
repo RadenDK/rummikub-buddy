4e7b9d1010f546879505cb7d400ca25863d64792
Rasmus <rasmusjensen.dev@gmail.com>
2025-02-21 10:41:06 +0100

    pressed ctrl alt L

--- laravel-client/resources/views/dashboard.blade.php
+++ laravel-client/resources/views/dashboard.blade.php
@@ -16,68 +16,67 @@
 </head>
 
 <body>
-    <div class="dashboard-container">
-        <!-- Profile Icon -->
-        <div class="profile-icon">
-            <img src="{{ session('google_user')['avatar'] }}" alt="Profile Icon" class="profile-img">
-        </div>
-
-        <!-- Players Table -->
-        <div class="players-container">
-            <!-- Row containing the button and table -->
-            <div class="header-row">
-                <!-- Add Player Button -->
-                <button id="add-player-btn" class="add-btn">+</button>
-            </div>
-            <table id="score-table">
-                <thead>
-                    <tr id="player-row">
-                        <th id="static-header">#</th>
-                        <!-- Dynamic players will be added here -->
-                    </tr>
-                </thead>
-                <tbody id="rounds-body">
-                    <!-- Dynamic rounds will be added here -->
-                </tbody>
-                <tfoot>
-                    <tr id="totals-row">
-                        <th>Total</th>
-                        <!-- Dynamic total scores will be added here -->
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-
-        <!-- Add Round Button -->
-        <button id="add-round-btn" class="add-btn">Add Round</button>
-
-        <!-- Finish Button -->
-        <div class="finish-game-container">
-            <button id="finish-game-btn" class="finish-btn">Finish Game</button>
-        </div>
+<div class="dashboard-container">
+    <!-- Profile Icon -->
+    <div class="profile-icon">
+        <img src="{{ session('google_user')['avatar'] }}" alt="Profile Icon" class="profile-img">
     </div>
 
-    <!-- Modal Window -->
-    <div id="finish-game-modal" class="modal">
-        <div class="modal-content">
-            <h2>Finish Game</h2>
-            <p>Are you sure you want to finish this game?</p>
-            <div class="modal-buttons">
-                <button id="abort-game-btn" class="modal-btn abort-btn">Abort Game</button>
-                <button id="save-game-btn" class="modal-btn save-btn">Save Game</button>
-            </div>
+    <!-- Players Table -->
+    <div class="players-container">
+        <!-- Row containing the button and table -->
+        <div class="header-row">
+            <!-- Add Player Button -->
+            <button id="add-player-btn" class="add-btn">+</button>
         </div>
+        <table id="score-table">
+            <thead>
+            <tr id="player-row">
+                <th id="static-header">#</th>
+                <!-- Dynamic players will be added here -->
+            </tr>
+            </thead>
+            <tbody id="rounds-body">
+            <!-- Dynamic rounds will be added here -->
+            </tbody>
+            <tfoot>
+            <tr id="totals-row">
+                <th>Total</th>
+                <!-- Dynamic total scores will be added here -->
+            </tr>
+            </tfoot>
+        </table>
     </div>
 
-    <!-- Link to JS -->
-    <script type="module" src="{{ asset('js/main.js') }}"></script>
+    <!-- Add Round Button -->
+    <button id="add-round-btn" class="add-btn">Add Round</button>
+
+    <!-- Finish Button -->
+    <div class="finish-game-container">
+        <button id="finish-game-btn" class="finish-btn">Finish Game</button>
+    </div>
+</div>
+
+<!-- Modal Window -->
+<div id="finish-game-modal" class="modal">
+    <div class="modal-content">
+        <h2>Finish Game</h2>
+        <p>Are you sure you want to finish this game?</p>
+        <div class="modal-buttons">
+            <button id="abort-game-btn" class="modal-btn abort-btn">Abort Game</button>
+            <button id="save-game-btn" class="modal-btn save-btn">Save Game</button>
+        </div>
+    </div>
+</div>
+
+<!-- Link to JS -->
+<script type="module" src="{{ asset('js/main.js') }}"></script>
 </body>
 
 <script>
     const googleFullName = {!! json_encode(session('google_user')['name'] ?? null) !!};
     const googleFirstName = googleFullName ? googleFullName.split(' ')[0] : null;
 </script>
-
 
 
 </html>
