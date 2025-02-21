f1f27da851e9d7c170f1bb41b5a6096c52032745
Rasmus <rasmusjensen.dev@gmail.com>
2025-02-21 10:40:47 +0100

    Made a PlayerService

---
+++ laravel-client/app/Service/PlayerService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Service;
+
+use GuzzleHttp\Exception\GuzzleException;
+
+class PlayerService
+{
+
+    protected ApiService $apiService;
+
+    public function __construct(ApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
+    public function saveMainPlayer(string $email, string $name): array
+    {
+        $baseUrl = env('API_BASE_ENDPOINT');
+        $endpoint = "/players";
+        $url = $baseUrl . $endpoint;
+
+        $options = [
+            'json' => [
+                'email' => $email,
+                'name' => $name
+            ]
+        ];
+
+        try {
+            $response = $this->apiService->postRequest($url, $options);
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            return [];
+        }
+    }
+
+    public function getListOfPriorUsedPlayers(string $email): array
+    {
+        $baseUrl = env('API_BASE_ENDPOINT');
+        $endpoint = "/players/$email";
+        $url = $baseUrl . $endpoint;
+
+        try {
+            $response = $this->apiService->getRequest($url);
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            return [];
+        }
+    }
+
+}
