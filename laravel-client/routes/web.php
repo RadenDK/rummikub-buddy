68b38338178a37a3317423deb1bdb4707581c73c
Rasmus <rasmusjensen.dev@gmail.com>
2025-03-10 20:03:01 +0100

    test to see if web client container actually works

--- laravel-client/routes/web.php
+++ laravel-client/routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Middleware\CheckLogin;
 
-Route::get('/', [LoginController::class, 'index']);
+//Route::get('/', [LoginController::class, 'index']);
+Route::get('/', function () {return "Hello world";});
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
