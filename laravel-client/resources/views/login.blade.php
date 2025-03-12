c25a74c6f106e5924c8dc665b2569e55ed782af7
Rasmus <rasmusjensen.dev@gmail.com>
2025-03-12 20:21:47 +0100

    trying to get css to work

--- laravel-client/resources/views/login.blade.php
+++ laravel-client/resources/views/login.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
 <div class="login-container">
-    <img src="{{ asset('images/rummikub-logo.png') }}" alt="Rummikub Logo">
+    <img src="images/rummikub-logo.png" alt="Rummikub Logo">
     <h1>Welcome to Rummikub Buddy</h1>
     <a href="/auth/google" class="login-btn">Login with Google</a>
 </div>
